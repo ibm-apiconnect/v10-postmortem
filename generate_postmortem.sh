786184d3ba0633c774352d2c6f867d3b070d4a83
reginarichardson <reginarichardson@ibm.com>
2023-07-24 15:04:59 -0700

    Removed the individual variables to simply the code

--- generate_postmortem.sh
+++ generate_postmortem.sh
@@ -250,23 +250,15 @@ mkdir -p $TEMP_PATH
 kubectl get pods --all-namespaces 2>/dev/null | egrep -q "metrics-server|openshift-monitoring"
 OUTPUT_METRICS=$?
 
+#Namespaces
+for NAMESPACE_OPTIONS in "rook-ceph" "rook-ceph-system" "ibm-common-services" "openshift-marketplace" "openshift-operators" "openshift-operator-lifecycle-manager" ;
+    do 
+        kubectl get ns 2>/dev/null | grep -q "$NAMESPACE_OPTIONS"
+        if [[ $? -eq 0 && $SPECIFIC_NAMESPACES -ne 1 ]]; then
+            NAMESPACE_LIST+=" $NAMESPACE_OPTIONS"
+        fi
+    done
 
-#Namespaces 
-ROOK_CEPH="rook-ceph"
-ROOK_CEPH_SYSTEM="rook-ceph-system"
-IBM_COMMON_SERVICES="ibm-common-services" 
-OPENSHIFT_MARKETPLACE="openshift-marketplace" 
-OPENSHIFT_OPERATORS="openshift-operators"
-OPENSHIFT_OPERATOR_LIFECYCLE_MANAGER="openshift-operator-lifecycle-manager"
-
-NAMESPACE_OPTIONS=("ROOK_CEPH" "ROOK_CEPH_SYSTEM" "IBM_COMMON_SERVICES" "OPENSHIFT_MARKETPLACE" "OPENSHIFT_OPERATORS" "OPENSHIFT_OPERATOR_LIFECYCLE_MANAGER")
-for CURRENT_NAMESPACE in "${NAMESPACE_OPTIONS[@]}"; do 
-    NAMESPACE_RUNNING="${!CURRENT_NAMESPACE}"
-    kubectl get ns 2>/dev/null | grep -q "$NAMESPACE_RUNNING"
-    if [[ $? -eq 0 && $SPECIFIC_NAMESPACES -ne 1 ]]; then
-        NAMESPACE_LIST+=" $NAMESPACE_RUNNING"
-    fi
-done
 
 #================================================= pull ova data =================================================
 if [[ $IS_OVA -eq 1 ]]; then
