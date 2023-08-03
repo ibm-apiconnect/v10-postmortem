83599bcb610ac5e79e9d4d17a34fad7b933668ea
reginarichardson <reginarichardson@ibm.com>
2023-08-03 11:26:56 -0700

    Include output of kubectl api-resources
    Refs: https://github.ibm.com/velox/platform/issues/8529
    
    - Added section to include kubectl api-resources
    - Extra information is captured under kubernetes/cluster/lists
    - If the command doesn't exit with zero then the file is never created and therefore is not saved

--- generate_postmortem.sh
+++ generate_postmortem.sh
@@ -670,6 +670,11 @@ if [[ $? -eq 0 && ${#OUTPUT} -gt 0 ]]; then
     echo "$OUTPUT" > "${K8S_CLUSTER_LIST_DATA}/icsp.yaml"
 fi
 
+#Get api-resources
+OUTPUT=`$KUBECTL api-resources 2>/dev/null`
+if [[ $? -eq 0 && ${#OUTPUT} -gt 0 ]]; then 
+    echo "$OUTPUT" > "${K8S_CLUSTER_LIST_DATA}/api-resources.out"
+fi
 #------------------------------------------------------------------------------------------------------
 
 #---------------------------------- collect namespace specific data -----------------------------------
