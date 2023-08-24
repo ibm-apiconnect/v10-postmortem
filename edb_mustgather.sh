b78e5e89c89069c2c8a9e51eea2257fd4b58d20e
William McEnery <william.mcenery1@ibm.com>
2023-08-24 10:59:09 +0100

    Collect edb operator info when deployed in a different namespace. Redirect edb mustgather logs to a file.

--- edb_mustgather.sh
+++ edb_mustgather.sh
@@ -3,6 +3,19 @@
 # all logs collected are based off the EDB's troubleshooting guide
 # https://docs.enterprisedb.io/cloud-native-postgresql/1.15.1/troubleshooting/#troubleshooting
 
+# Confirm whether oc or kubectl exists and choose which command tool to use based on that
+which oc &> /dev/null
+if [[ $? -eq 0 ]]; then
+    KUBECTL="oc"
+else
+    which kubectl &> /dev/null
+    if [[ $? -ne 0 ]]; then
+        echo "Unable to locate the command [kubectl] nor [oc] in the path.  Either install or add it to the path.  EXITING..."
+        exit 1
+    fi
+    KUBECTL="kubectl"
+fi
+
 EDB_CLUSTER_NAMESPACE=$1
 LOG_PATH=$2
 CNP_INSTALLED=false
@@ -23,11 +36,21 @@ if [ -z "$1" ] || [ -z "$2" ]
     exit 1
 fi
 
-EDB_OP_NAMESPACE='ibm-common-services'
-PG_OP=$(oc get po -n ${EDB_OP_NAMESPACE} -o=custom-columns=NAME:.metadata.name | grep postgresql-operator-controller-manager)
-EDB_CLUSTER_NAME=$(oc get cluster -n ${EDB_CLUSTER_NAMESPACE} -o=jsonpath='{.items[0].metadata.name}')
-EDB_POD_NAMES=$(kubectl get pod -l k8s.enterprisedb.io/cluster=${EDB_CLUSTER_NAME} -L role -n ${EDB_CLUSTER_NAMESPACE} -o=custom-columns=NAME:.metadata.name --no-headers)
-EDB_BACKUP_NAMES=$(kubectl get backups -l k8s.enterprisedb.io/cluster=${EDB_CLUSTER_NAME} -L role -n ${EDB_CLUSTER_NAMESPACE} -o=custom-columns=NAME:.metadata.name --no-headers)
+EDB_OP_NAMESPACE=''
+PG_OP=''
+ARCHITECTURE=$($KUBECTL get nodes -o jsonpath='{.items[0].status.nodeInfo.architecture}')
+
+if [ "$ARCHITECTURE" = 's390x' ]; then
+    EDB_OP_NAMESPACE='ibm-common-services'
+    PG_OP=$($KUBECTL get po -n ${EDB_OP_NAMESPACE} -o=custom-columns=NAME:.metadata.name | grep postgresql-operator-controller-manager)
+else
+    EDB_OP_NAMESPACE=$EDB_CLUSTER_NAMESPACE
+    PG_OP=$($KUBECTL get po -n ${EDB_OP_NAMESPACE} -o=custom-columns=NAME:.metadata.name | grep edb-operator)
+fi
+
+EDB_CLUSTER_NAME=$($KUBECTL get cluster -n ${EDB_CLUSTER_NAMESPACE} -o=jsonpath='{.items[0].metadata.name}')
+EDB_POD_NAMES=$($KUBECTL get pod -l k8s.enterprisedb.io/cluster=${EDB_CLUSTER_NAME} -L role -n ${EDB_CLUSTER_NAMESPACE} -o=custom-columns=NAME:.metadata.name --no-headers)
+EDB_BACKUP_NAMES=$($KUBECTL get backups -l k8s.enterprisedb.io/cluster=${EDB_CLUSTER_NAME} -L role -n ${EDB_CLUSTER_NAMESPACE} -o=custom-columns=NAME:.metadata.name --no-headers)
 K8S_DATA="${TEMP_PATH}/kubernetes"
 K8S_NAMESPACES="${K8S_DATA}/namespaces"
 K8S_NAMESPACES_SPECIFIC="${K8S_NAMESPACES}/${EDB_OP_NAMESPACE}"
@@ -48,7 +71,6 @@ mkdir ${NS}
 mkdir ${SPECIFIC_NS_EDB_OP}
 mkdir ${SPECIFIC_NS_CLUSTER}
 mkdir ${PO}
-mkdir ${PO}/${PG_OP}
 mkdir ${CLUSTER}
 mkdir ${CLUSTER}/${EDB_CLUSTER_NAME}
 mkdir ${CLUSTER_PODS}
@@ -56,39 +78,45 @@ mkdir ${CLUSTER_BACKUPS}
 mkdir ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}
 
 function gatherEdbOperatorData() {
-    kubectl describe pod ${PG_OP} -n ${EDB_OP_NAMESPACE} > ${PO}/${PG_OP}/describe.txt
-    kubectl logs ${PG_OP} -n ${EDB_OP_NAMESPACE} > ${PO}/${PG_OP}/logs.txt
+    for pod in $PG_OP
+    do
+        mkdir ${PO}/${pod}
+        $KUBECTL get po ${pod} -o yaml -n ${EDB_OP_NAMESPACE} > ${PO}/${pod}/pod.yaml
+        $KUBECTL describe pod ${pod} -n ${EDB_OP_NAMESPACE} > ${PO}/${pod}/describe.txt
+        $KUBECTL logs ${pod} -n ${EDB_OP_NAMESPACE} > ${PO}/${pod}/logs.txt
+        $KUBECTL logs ${pod} -n ${EDB_OP_NAMESPACE} --previous 2>/dev/null > ${PO}/${pod}/previous-logs.txt
+    done
 }
 
 function gatherClusterData() {
     if [ "$CNP_INSTALLED" = true ]; then
-        kubectl cnp status ${EDB_CLUSTER_NAME} -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/status.txt
-        kubectl cnp status ${EDB_CLUSTER_NAME} --verbose -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/status-verbose.txt
+        $KUBECTL cnp status ${EDB_CLUSTER_NAME} -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/status.txt
+        $KUBECTL cnp status ${EDB_CLUSTER_NAME} --verbose -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/status-verbose.txt
     fi
-    kubectl get cluster -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/info.txt
-    kubectl get cluster -o yaml -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/cluster.yaml
-    kubectl describe cluster -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/describe.txt
+    $KUBECTL get cluster -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/info.txt
+    $KUBECTL get cluster -o yaml -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/cluster.yaml
+    $KUBECTL describe cluster -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER}/${EDB_CLUSTER_NAME}/describe.txt
 
 }
 
 function gatherEDBPodData() {
-    kubectl get pod -l k8s.enterprisedb.io/cluster=${EDB_CLUSTER_NAME} -L role -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_PODS}/pods.txt
+    $KUBECTL get pod -l k8s.enterprisedb.io/cluster=${EDB_CLUSTER_NAME} -L role -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_PODS}/pods.txt
     for pod in ${EDB_POD_NAMES}; do
-        mkdir ${CLUSTER_PODS}/${pod} 
-        kubectl get po ${pod} -o yaml -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_PODS}/${pod}/pod.yaml
-        kubectl describe pod ${pod} -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_PODS}/${pod}/describe.txt
-        kubectl logs ${pod} -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_PODS}/${pod}/logs.txt
-        kubectl logs ${pod} -n ${EDB_CLUSTER_NAMESPACE} --previous 2>/dev/null > ${CLUSTER_PODS}/${pod}/previous-logs.txt
-        kubectl logs ${pod} -n ${EDB_CLUSTER_NAMESPACE} | jq -r '.record | select(.error_severity == "FATAL")' > ${CLUSTER_PODS}/${pod}/logs-fatal.txt
+        mkdir ${CLUSTER_PODS}/${pod}
+        $KUBECTL get po ${pod} -o yaml -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_PODS}/${pod}/pod.yaml
+        $KUBECTL describe pod ${pod} -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_PODS}/${pod}/describe.txt
+        $KUBECTL logs ${pod} -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_PODS}/${pod}/logs.txt
+        $KUBECTL logs ${pod} -n ${EDB_CLUSTER_NAMESPACE} --previous 2>/dev/null > ${CLUSTER_PODS}/${pod}/previous-logs.txt
+        $KUBECTL logs ${pod} -n ${EDB_CLUSTER_NAMESPACE} | jq -r '.record | select(.error_severity == "FATAL")' > ${CLUSTER_PODS}/${pod}/logs-fatal.txt
     done
 }
 
 function gatherEDBBackupData() {
-    kubectl get backups -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}/backups.txt
+    $KUBECTL get backups -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}/backups.txt
     for backup in ${EDB_BACKUP_NAMES}; do
-        mkdir ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}/${backup} 
-        kubectl get backups ${backup} -o yaml -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}/${backup}/backup.yaml
-        kubectl describe backups ${backup} -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}/${backup}/describe.txt
+        mkdir ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}/${backup}
+        $KUBECTL get backups ${backup} -o yaml -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}/${backup}/backup.yaml
+        $KUBECTL describe backups ${backup} -n ${EDB_CLUSTER_NAMESPACE} > ${CLUSTER_BACKUPS}/${EDB_CLUSTER_NAME}/${backup}/describe.txt
     done
 }
 
