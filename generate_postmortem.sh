6c71021bb4621df30bf7a875b9454eb56e7f3218
Regina Richardson <ReginaRichardson@ibm.com>
2024-02-22 11:59:52 -0800

    Changed variables to be not so when they are set to 1 not ___ is true

--- generate_postmortem.sh
+++ generate_postmortem.sh
@@ -139,22 +139,22 @@ for switch in $@; do
             NAMESPACE_LIST="kube-system"
             ;;
         *"--no-diagnostic"*)
-            DIAG_GATEWAY=1
-            DIAG_MANAGER=1 
-            DIAG_PORTAL=1
-            DIAG_ANALYTICS=1 
+            NOT_DIAG_GATEWAY=1
+            NOT_DIAG_MANAGER=1 
+            NOT_DIAG_PORTAL=1 
+            NOT_DIAG_ANALYTICS=1 
             ;;
         *"--no-gateway-diagnostic"*)
-            DIAG_GATEWAY=1 
+            NOT_DIAG_GATEWAY=1
             ;;
         *"--no-manager-diagnostic"*)
-            DIAG_MANAGER=1 
+            NOT_DIAG_MANAGER=1 
             ;;
         *"--no-portal-diagnostic"*)
-            DIAG_PORTAL=1 
+            NOT_DIAG_PORTAL=1 
             ;;
         *"--no-analytics-diagnostic"*)
-            DIAG_ANALYTICS=1 
+            NOT_DIAG_ANALYTICS=1 
             ;;
         *"--log-limit"*)
             limit=`echo "${switch}" | cut -d'=' -f2`
@@ -257,7 +257,7 @@ if [[ $? -ne 0 ]]; then
 fi
 
 #Checking for diagnostic collection, ensuring the crunchy script or edb script are available
-if [[ ! $DIAG_MANAGER -eq 1 ]]; then
+if [[ ! $NOT_DIAG_MANAGER -eq 1 ]]; then
     EDB_CLUSTER_NAME=$($KUBECTL get cluster --all-namespaces -o=jsonpath='{.items[0].metadata.name}' 2>/dev/null)
     if [[ -z "$EDB_CLUSTER_NAME" ]]; then
         COLLECT_CRUNCHY=1
@@ -1403,7 +1403,7 @@ for NAMESPACE in $NAMESPACE_LIST; do
             fi
 
             #grab postgres data
-            if [[ ! $DIAG_MANAGER -eq 1 && $COLLECT_CRUNCHY -eq 1 && "$status" == "Running" && "$pod" == *"postgres"* && ! "$pod" =~ (backrest|pgbouncer|stanza|operator) ]]; then
+            if [[ ! $NOT_DIAG_MANAGER -eq 1 && $COLLECT_CRUNCHY -eq 1 && "$status" == "Running" && "$pod" == *"postgres"* && ! "$pod" =~ (backrest|pgbouncer|stanza|operator) ]]; then
                 target_dir="${K8S_NAMESPACES_POD_DIAGNOSTIC_DATA}/postgres/${pod}-pglogs"
                 health_dir="${K8S_NAMESPACES_POD_DIAGNOSTIC_DATA}/postgres/${pod}-health-stats"
 
@@ -1488,7 +1488,7 @@ for NAMESPACE in $NAMESPACE_LIST; do
             fi
 
             #grab gateway diagnostic data
-            if [[ ! $DIAG_GATEWAY -eq 1 && $IS_GATEWAY -eq 1 && $ready -eq 1 && "$status" == "Running" && "$pod" != *"monitor"* && "$pod" != *"operator"* ]]; then
+            if [[ ! $NOT_DIAG_GATEWAY -eq 1 && $IS_GATEWAY -eq 1 && $ready -eq 1 && "$status" == "Running" && "$pod" != *"monitor"* && "$pod" != *"operator"* ]]; then
                 GATEWAY_DIAGNOSTIC_DATA="${K8S_NAMESPACES_POD_DIAGNOSTIC_DATA}/gateway/${pod}"
                 mkdir -p $GATEWAY_DIAGNOSTIC_DATA
 
@@ -1572,7 +1572,7 @@ for NAMESPACE in $NAMESPACE_LIST; do
             fi
 
             #grab analytics diagnostic data
-            if [[ ! $DIAG_ANALYTICS -eq 1 && $IS_ANALYTICS -eq 1 && $ready -eq 1 && "$status" == "Running" ]]; then
+            if [[ ! $NOT_DIAG_ANALYTICS -eq 1 && $IS_ANALYTICS -eq 1 && $ready -eq 1 && "$status" == "Running" ]]; then
                 ANALYTICS_DIAGNOSTIC_DATA="${K8S_NAMESPACES_POD_DIAGNOSTIC_DATA}/analytics/${pod}"
                 mkdir -p $ANALYTICS_DIAGNOSTIC_DATA
 
@@ -1608,7 +1608,7 @@ for NAMESPACE in $NAMESPACE_LIST; do
                 [[ $? -eq 0 && -s "${LOG_TARGET_PATH}/${pod}_${container}_previous.log" ]] || rm -f "${LOG_TARGET_PATH}/${pod}_${container}_previous.log"
 
                 #grab portal data
-                if [[ ! $DIAG_PORTAL -eq 1 && $IS_PORTAL -eq 1 && "$status" == "Running" ]]; then
+                if [[ ! $NOT_DIAG_PORTAL -eq 1 && $IS_PORTAL -eq 1 && "$status" == "Running" ]]; then
                     PORTAL_DIAGNOSTIC_DATA="${K8S_NAMESPACES_POD_DIAGNOSTIC_DATA}/portal/${pod}/${container}"
 
                     echo "${pod}" | grep -q "www"
